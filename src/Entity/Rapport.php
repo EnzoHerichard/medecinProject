e5aae393052a1da123ed611ce43eda6cba59f19d
EnzoHerichard <herichardenzo@gmail.com>
2022-02-22 18:24:37 +0100

    Création des entités

---
+++ src/Entity/Rapport.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RapportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RapportRepository::class)
+ */
+class Rapport
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bilan;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Visiteur::class, inversedBy="rapports")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $visiteur;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Medecin::class, inversedBy="rapport")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $medecin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBilan(): ?string
+    {
+        return $this->bilan;
+    }
+
+    public function setBilan(string $bilan): self
+    {
+        $this->bilan = $bilan;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getVisiteur(): ?Visiteur
+    {
+        return $this->visiteur;
+    }
+
+    public function setVisiteur(?Visiteur $visiteur): self
+    {
+        $this->visiteur = $visiteur;
+
+        return $this;
+    }
+
+    public function getMedecin(): ?Medecin
+    {
+        return $this->medecin;
+    }
+
+    public function setMedecin(?Medecin $medecin): self
+    {
+        $this->medecin = $medecin;
+
+        return $this;
+    }
+}
